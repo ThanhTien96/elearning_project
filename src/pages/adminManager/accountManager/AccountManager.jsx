eb2900a459d50a979c7c58593d8d1cb980448345
ThanhTien <thanhtien2094@gmail.com>
2023-01-17 06:30:51 +0700

    'fixButtonAddAccount'

--- src/pages/adminManager/accountManager/AccountManager.jsx
+++ src/pages/adminManager/accountManager/AccountManager.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { getApiAccount } from '../../../redux/reducer/admin/accountManagerSlice';
 import {  fetchApiAccountAction } from '../../../redux/action/adminAction/accountManagerAction';
-
+import '../../../utils/mixin.scss'
 
 const AccountManager = (props) => {
 
@@ -21,7 +21,7 @@ const AccountManager = (props) => {
     {
       title: 'Họ Tên',
       dataIndex: 'hoTen',
-      key: '3',
+      key: '1',
       sorter: (a, b) => {
         let tenA = a.hoTen.toLowerCase().trim();
         let tenB = b.hoTen.toLowerCase().trim();
@@ -33,7 +33,7 @@ const AccountManager = (props) => {
       },
       render: (text, user) => {
         return <Fragment>
-          <h4 className='text-green-700'>{user.hoTen}</h4>
+          <h4 className='gradientText font-medium text-lg'>{user.hoTen}</h4>
         </Fragment>
       },
       sortDirection: ['descend', 'ascend'],
@@ -41,9 +41,9 @@ const AccountManager = (props) => {
     },
     {
       title: 'Tài Khoản',
-      key: '4',
+      key: '2',
       dataIndex: 'taiKhoan',
-      width: '15%',
+      width: '20%',
       render: (text, user, index) => {
         return <Fragment>
           <p>{user.taiKhoan}</p>
@@ -53,9 +53,9 @@ const AccountManager = (props) => {
     },
     {
       title: 'Số Điện Thoại',
-      key: '4',
+      key: '3',
       dataIndex: 'soDT',
-      width: '20%',
+      width: '10%',
       render: (text, user, index) => {
         return <Fragment>
           <p>{user.soDT}</p>
@@ -63,30 +63,19 @@ const AccountManager = (props) => {
       }
     },
     {
-      title: 'Mã Loại Người Dùng',
+      title: 'Loại Người Dùng',
       key: '4',
-      dataIndex: 'maLoaiNguoiDung',
-      width: '20%',
+      dataIndex: 'tenLoaiNguoiDung',
+      width: '10%',
       render: (text, user, index) => {
         return <Fragment>
-          <p>{user.maLoaiNguoiDung}</p>
-        </Fragment>
-      }
-    },
-    {
-      title: 'Mã Nhóm',
-      key: '4',
-      dataIndex: 'maNhom',
-      width: '20%',
-      render: (text, user, index) => {
-        return <Fragment>
-          <p>{user.maNhom}</p>
+          <p>{user.tenLoaiNguoiDung}</p>
         </Fragment>
       }
     },
     {
       title: 'Email',
-      key: '4',
+      key: '5',
       dataIndex: 'email',
       width: '20%',
       render: (text, user, index) => {
@@ -95,6 +84,18 @@ const AccountManager = (props) => {
         </Fragment>
       }
     },
+    {
+      title: 'Tác Vụ',
+      key: '6',
+      dataIndex: 'maLoaiNguoiDung',
+      width: '20%',
+      render: (text, user, index) => {
+        return <Fragment>
+          <p>thêm tác vụ ở đây</p>
+        </Fragment>
+      }
+    },
+
 
 
   ];
@@ -118,14 +119,14 @@ const AccountManager = (props) => {
   return (
     <div>
       <div className="flex justify-between mb-5 md:mb-10" > 
-        <h3 className='text-orange-600 text-xl'>Quản lý Tài Khoản Người Dùng</h3>
+        <h3 className='text-teal-600 text-xl font-semibold'>Quản lý Tài Khoản</h3>
         <Search
             allowClear
             className='w-1/2'
             placeholder="Nhập từ khóa tìm kiếm"
             // onSearch={onSearch}
         />
-        <Button onClick={() => navigate('/admin/account/create')} type='primary' size='large'><UserOutlined />Thêm Tài Khoản</Button>
+        <Button className='btnGradient flex items-center' onClick={() => navigate('/admin/account/create')} type='primary' size='large'><UserOutlined /> <span>Thêm Tài Khoản</span></Button>
 
       </div>
       <Table pagination={{ total: accountList?.totalCount }} rowKey={'taiKhoan'} columns={columns} dataSource={data} onChange={onChange} />
